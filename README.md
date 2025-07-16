3a2b46cb976e951a9998483ba160441a5c21c38c
Biribi Africa <biribiafrica@gmail.com>
2025-07-16 05:20:09 -0400

    Fresh deployable frontend with working build

---
+++ README.md
@@ -0,0 +1,2 @@
+# BIRIBI Frontend
+This is the React frontend for BIRIBI classifieds.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to BIRIBI</h1>
+      <p>Your trusted classifieds for Ghana and West Africa</p>
+    </div>
+  );
+}
+
+export default App;
